5dfcdb0875d739e834f542691be73f474414bc4b
Anorme <anormeinkumsah@gmail.com>
2025-07-08 14:36:15 +0000

    chore(lint): configure eslint to work with prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"] 
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "commonjs" 
+    } 
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    languageOptions: { 
+      globals: globals.node 
+    } 
+  },
+  { 
+    files: ["**/*.json"], 
+    plugins: { json }, 
+    language: "json/json", 
+    extends: ["json/recommended"] 
+  },
+  eslintConfigPrettier
+]);
