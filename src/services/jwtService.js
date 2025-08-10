9d81d182dd3d1c27f063d6b1f0877c8b4c646c38
Anorme <anormeinkumsah@gmail.com>
2025-08-10 00:47:46 +0000

    chore(logs): cleanup troubleshooting log

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -2,7 +2,6 @@ import env from "../config/env.js";
 import jwt from "jsonwebtoken";
 
 function signToken(payload) {
-  console.log("Signing token expires in: ", env.JWT_EXPIRES_IN);
   return jwt.sign(payload, env.JWT_SECRET, {
     expiresIn: Number(env.JWT_EXPIRES_IN) || 3600,
   });
