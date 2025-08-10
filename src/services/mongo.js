cecd0315ea957bf23cd35613696fe2024a998e51
Anorme <anormeinkumsah@gmail.com>
2025-08-10 00:54:22 +0000

    feat(logging): use custom logger utility for more traceable logging

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -2,14 +2,16 @@ import env from "../config/env.js";
 
 import mongoose from "mongoose";
 
+import logger from "../config/logger.js";
+
 const MONGO_URL = env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
-  console.log("Mongoose connection ready!");
+  logger.info("Mongoose connection ready!");
 });
 
 mongoose.connection.on("error", err => {
-  console.error("Error connecting to mongoose", err);
+  logger.error("Error connecting to mongoose", err);
 });
 
 async function mongoConnect() {
