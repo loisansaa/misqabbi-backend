ccf52bb9c8999b041a351ebf84b216758d13e1d0
Anorme <anormeinkumsah@gmail.com>
2025-08-10 01:06:25 +0000

    feat(error handling): log enriched context with conditional stack trace in dev

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -1,13 +1,25 @@
+import logger from "../config/logger.js";
+import env from "../config/env.js";
+
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
-  console.error(err.stack); //Log to file or service at scale
+  const { NODE_ENV } = env;
+  const isDev = NODE_ENV === "development";
 
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
+  logger.error(err.message, {
+    method: req.method,
+    path: req.originalUrl,
+    user: req.user?.id || "anonymous",
+    statusCode: statusCode,
+    ...(isDev && { stack: err.stack }),
+  });
+
   res.status(statusCode).json({
     message,
-    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+    ...(isDev && { stack: err.stack }),
   });
 }
 
