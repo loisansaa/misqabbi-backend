e67d5b9c0d1eb401391477ddf9e10358049cc875
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-08-13 17:53:10 +0000

    feat: productValidator created

--- src/validators/product.validator.js
+++ src/validators/product.validator.js
@@ -1,3 +1,6 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
 /**
  * Joi validation schema for Product creation/update.
  *
@@ -11,3 +14,32 @@
  * - isPublished: optional, boolean
  * - createdBy: optional, valid ObjectId string (references User)
  */
+
+export const productValidator = Joi.object({
+  name: Joi.string().trim().required().messages({
+    "string.empty": "Product name is required",
+  }),
+  description: Joi.string().trim().optional(),
+  price: Joi.number().min(0).required().messages({
+    "number.min": "Price must be at least 0",
+    "any.required": "Price is required",
+  }),
+  images: Joi.array().items(Joi.string().uri()).max(5).optional().messages({
+    "array.max": "Maximum of 5 images allowed",
+    "string.uri": "Invalid image URL format",
+  }),
+  category: Joi.string().lowercase().trim().required(),
+  stock: Joi.number().min(0).required().messages({
+    "number.min": "Stock must be at least 0",
+    "any.required": "Stock is required",
+  }),
+  isPublished: Joi.boolean().optional(),
+  createdBy: Joi.string()
+    .custom((value, helpers) => {
+      if (!mongoose.Types.ObjectId.isValid(value)) {
+        return helpers.error("any.invalid");
+      }
+      return value; // must return the value if valid
+    }, "ObjectId Validation")
+    .optional(),
+});
