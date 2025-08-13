d8d9a353d8b3c60630727222c822a5dca5a0b6cf
loisansaa <117663405+loisansaa@users.noreply.github.com>
2025-08-13 17:59:20 +0000

    feat: validator middleware updated with order and product validation logic

--- src/middleware/validator.middleware.js
+++ src/middleware/validator.middleware.js
@@ -1,7 +1,29 @@
 import { userValidator } from "../validators/user.validator.js";
+import { productValidator } from "../validators/product.validator.js";
+import { orderValidator } from "../validators/order.validator.js";
 
 export function validateUser(req, res, next) {
   const { error } = userValidator.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      errors: error.details.map(err => err.message),
+    });
+  }
+  next();
+}
+
+export function validateProduct(req, res, next) {
+  const { error } = productValidator.validate(req.body, { abortEarly: false });
+  if (error) {
+    return res.status(400).json({
+      errors: error.details.map(err => err.message),
+    });
+  }
+  next();
+}
+
+export function validateOrder(req, res, next) {
+  const { error } = orderValidator.validate(req.body, { abortEarly: false });
   if (error) {
     return res.status(400).json({
       errors: error.details.map(err => err.message),
